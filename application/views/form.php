d8c0b061bece46875c7da484e309cf854954ecee
dianaameliasafitri <dianaameliasafitri7@gmail.com>
2018-05-11 20:19:07 +0700

    update

---
+++ application/views/form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Input - User  </title>
+</head>
+<body>
+
+<center>
+	<h1>Login</h1>
+
+
+	<form action="http://localhost/kuytravel/index.php/User/masukkan" method="post">
+	
+		<table>
+			<tr>
+				<td>username</td>
+				<td><input type="text" name="Username"></td>
+			</tr>
+			<tr>
+				<td>password</td>
+				<td><input type="password" name="Password"></td>
+			</tr>
+			<tr>
+				<td>fullname</td>
+				<td><input type="text" name="FullName"></td>
+			</tr>
+			<tr>
+				<td>level</td>
+				<td><input type="text" name="Level"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="login" value="Log In"></td>
+			</tr>
+		</table>
+	</form>
+		
+		
+	</form>
+</center>
+</body>
+</html>
